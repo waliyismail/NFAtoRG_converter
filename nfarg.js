bd0d1532e14c09a6a28a6e66f96a0f57610f9931
waliyismail <41659566+waliyismail@users.noreply.github.com>
2020-10-04 16:09:55 +0800

    done

--- nfarg.js
+++ nfarg.js
@@ -10,7 +10,7 @@ function createTable()
     var final_array = getArray(final.value);
     var alphabet = document.getElementById("alphabets");
     var alphabet_array = getArray(alphabet.value);
-    if(!alphabet_array.includes("&epsilon;")){alphabet_array.push("&epsilon;");}
+    if(!alphabet_array.includes("epsilon")){alphabet_array.push("epsilon");} // add epsilon if not given
     var table = document.getElementById("transition-table");
     var tt = document.getElementById("tt");
     var tablebut = document.getElementById("ttbut");
@@ -35,6 +35,7 @@ function createTable()
             {
                 //enter each states
                 // check if it is start or final states
+                // add arrow for start, asterisks for final
                 newTable += '<td><b>'
                 if(states_array[r-1] == start.value)
                 {
@@ -53,17 +54,26 @@ function createTable()
             }
             if(r == 0 && c > 0)
             {
-                newTable += '<td><b>' + alphabet_array[c-1] +'</b></td>';
+                // header for alphabet, change epsilon to $epsilon;
+                var a = "";
+                if(alphabet_array[c-1] === "epsilon") a = "&epsilon;";
+                else a = alphabet_array[c-1];
+                newTable += '<td><b>' + a.toLowerCase() +'</b></td>';
+
             }else{
-                // add dropdown containing the states
-                // id for each cells SA states alphabet
-                console.log(states_array[r-1]+alphabet_array[c-1]);
+                // add dropdown checkbox containing the states
+                // id for each cells SA (states)(alphabet)
+                //console.log(states_array[r-1]+alphabet_array[c-1]);
                 newTable += '<td>';
-                newTable += '<select id="' +states_array[r-1]+alphabet_array[c-1]+ '">';
+
+                newTable += '<select id="' +states_array[r-1]+alphabet_array[c-1]+ '">'; //html id cannot have special character
                 newTable += addOption(states_array);
                 // add Oslash
                 newTable+='<option selected value="empty">&Oslash;</option>';
                 newTable += '</select>'
+
+
+
                 newTable += '</td>';
                 //add options for states
             }
@@ -75,6 +85,22 @@ function createTable()
     tt.removeAttribute("hidden");
     tablebut.innerText = "Refresh table";
 ;    alert("Your input:\nStates: "+ states.value +"\nAlphabet: " + alphabet.value +"\nStart state: " + start.value +"\nFinal state: " + final.value );
+}
+
+function statesDropdown()
+{   
+    var r ='';
+    r+='<div class="dropdown">';
+    r+='<button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">';
+    r+='select class';
+    r+='</button>';
+    // r+='<div class="dropdown-menu" id="" aria-labelledby="dropdownMenuButton">';
+    //   <a class="dropdown-item" href="#">Action</a>
+    //   <a class="dropdown-item" href="#">Another action</a>
+    //   <a class="dropdown-item" href="#">Something else here</a>
+    //   <label class="dropdown-item"><input type="checkbox" name="" value="one">option one</label>
+
+    r+='</div></div>';
 }
 
 function checkform(form) {
@@ -98,7 +124,7 @@ function getArray(str)
     // remove all whitespace
     var newstr;
     newstr = str.replace(/\s/g,"$");
-    newstr = newstr.replace(/epsilon/,"&epsilon;");
+    //newstr = newstr.replace(/epsilon/,"&epsilon;");
     newstr = newstr.split("$");
     return newstr;
 }
@@ -121,34 +147,70 @@ function checkString()
 {
     var fdform = document.getElementById("fdform");
     //if(!checkform(fdform)){return};
-    var text = document.getElementById("stringCheck").value;
-    var strings = text.split(/\r?\n/);
-    if(strings.includes("epsilon"))
-    {
-        strings[strings.indexOf("epsilon")] = "&epsilon;"
-    }
+    var finalStates = getArray(document.getElementById("finalStates").value);
+    var startState = document.getElementById("startState");
+    var text = document.getElementById("stringCheck");
+    var strings = (text.value).split(/\r?\n/);
     //check from the table and move from next state
+    var t ='';
     for(s in strings)
     {
 
-        if(s!=null && acceptString(strings[s],0,0 ))
+        if(strings[s] === "epsilon")
+        {
+            //accept epsilon only if start is the final state right?
+            console.log("epsilon string")
+            if(finalStates.includes(startState))
+            {
+                t+= strings[s] +' is ok\n';
+            }else
+            {
+                t+= strings[s] +' is no\n';
+            }
+            continue;
+        }
+        if(acceptString(strings[s],0,0 ))
         {
             //TODO need to view this in the html
             console.log(strings[s] +" is ok");
+            t+= strings[s] +' is ok\n';
         }
         else
         {
             //TODO need to view this in the html
             console.log(strings[s] +" is no");
+            t+= strings[s] +' is no\n';
         }
     }
+    text.value = t;
+}
+function acceptString(str, alph_i, state_i)
+{
+    // if the char is the end of string
+    var states = getArray(document.getElementById("states").value);
+    var finalStates = getArray(document.getElementById("finalStates").value);
+    
+    var cell = document.getElementById(states[state_i] + str[alph_i]);
+    var cellValue = cell.options[cell.selectedIndex].value;
+    // console.log(states);
+    // console.log(finalStates);
+    // console.log(cellValue);
+    if (cellValue === "empty") return false;
+    
+    if(str.length == alph_i + 1)
+    {
+        //last alphabet must be in final states
+        if (finalStates.includes(cellValue)) return true;
+    }
+    // not a last string 
+    return acceptString(str, alph_i+1, states.indexOf(cellValue));
 }
 function convertToRg()
 {
     var states = getArray(document.getElementById("states").value);
     var finalStates = getArray(document.getElementById("finalStates").value);
     var alphabets = getArray(document.getElementById("alphabets").value);
-    if(!alphabets.includes("&epsilon;")){alphabets.push("&epsilon;");}
+    if(!alphabets.includes("epsilon")){alphabets.push("epsilon");}
     var rg = '';
 
     for(s in states)
@@ -158,9 +220,9 @@ function convertToRg()
         rg += states[s].toUpperCase() +' ' + rightArrow +' ';
         for(a in alphabets)
         {
-            console.log(states[s]+alphabets[a]);
+            //console.log(states[s]+alphabets[a]);
             var cell = document.getElementById(states[s] + alphabets[a]);
-            console.log(states[s] + alphabets[a] +"is" + cell)
+            //console.log(states[s] + alphabets[a] +"is" + cell)
             var cellValue = cell.options[cell.selectedIndex].value;
             if(cellValue != "empty")
             {
@@ -169,9 +231,9 @@ function convertToRg()
                     rg += " | ";
                 }
 
-                if(alphabets[a] === "&epsilon;")
+                if(alphabets[a] === "epsilon")
                 {
-                    rg += cellValue;
+                    rg += cellValue.toUpperCase();
                 }else{
 
                     rg += alphabets[a]+ cellValue.toUpperCase();
@@ -189,25 +251,4 @@ function convertToRg()
     d.innerHTML = rg;
     d.removeAttribute("hidden");
 ;
-}
-function acceptString(str, alph_i, state_i)
-{
-        // if the char is the end of string
-        var states = getArray(document.getElementById("states").value);
-        var finalStates = getArray(document.getElementById("finalStates").value);
-        
-        var cell = document.getElementById(states[state_i] + str[alph_i]);
-        var cellValue = cell.options[cell.selectedIndex].value;
-        // console.log(states);
-        // console.log(finalStates);
-        // console.log(cellValue);
-        if (cellValue === "empty") return false;
-        
-        if(str.length == alph_i + 1)
-        {
-            //last alphabet must be in final states
-            if (finalStates.includes(cellValue)) return true;
-        }
-        // not a last string 
-        return acceptString(str, alph_i+1, states.indexOf(cellValue));
 }
